4a620df9722f39bed951b66bc0e0cd553d227057
Brandon Ruffolo <brandon.ruffolo@mcgill.ca>
2021-10-13 17:49:45 -0400

    Adding serial functionality

--- Temperature Controller/PID/PID.ino
+++ Temperature Controller/PID/PID.ino
@@ -1,8 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
 #define BAUD 9600
-
-const double _tick = 1024000/16e6;  // period of a single clock cycle, in milliseconds
+#define THERMISTOR_PIN A2
  
 double temperature; 
 double setpoint;
@@ -23,6 +22,10 @@ char received_data[data_size];    // Array for storing received data
 char temp_data    [data_size];    // Temporary array for use when parsing
 char functionCall[20]  = {0};
 boolean newData = false;
+
+
+enum MODES{OPEN_LOOP,CLOSED_LOOP};
+enum MODES mode = OPEN_LOOP;
 
 
 void initialize(){
@@ -50,13 +53,34 @@ void loop() {
     }
 }
 
-void setPeriod(int period){     
+ISR(TIMER1_COMPA_vect){ 
+/* Timer1 compare interrupt 
+ * 
+ */
+  //control();   
+}
+
+
+char * strtokIndx;                    // This is used by strtok() as an index
+
+void parseData() {      // split the data into its parts
+    strtokIndx = strtok(temp_data,",");   // Get the first part - the string
+    strcpy(functionCall, strtokIndx);    // Copy it to function_call
+    
+  if(strcmp(functionCall,"set_parameters")  == 0){ set_parameters();  }
+  if(strcmp(functionCall,"set_dac_voltage") == 0){ set_dac_voltage(); }
+  if(strcmp(functionCall,"set_mode")==0){ set_mode(); }
+}
+
+
+
+void set_period(int period){     
 /*
  * Sets the time between calls to control().
  * 
  * period: period to be set in milliseconds.
  */    
-  unsigned int _number = floor(period / _tick) - 1; // Calculate the number of clock ticks in the specified period
+  unsigned int _number = floor(16e6*period/1024000) - 1; // Calculate the number of clock ticks in the specified period
   
   //char buf[256];
   //sprintf(buf,"DEBUG\tsetPeriod(%d): number = %d, tick = %ld us, actual period = %ld us\n", period, number, (long)(1e3*tick), (long)(1e3*tick*(number+1)));
@@ -72,35 +96,66 @@ void setPeriod(int period){
                             /* Note that OCR1a is a 16-bit register, so _number <= 65,535             */ 
 
   TCCR1B |= (1 << WGM12);                 // Enable clear timer on compare match (CTC) mode
-  TCCR1B |= (1 << CS12) | (1 << CS10);    // Set a prescaler of 1024 on Timer1 (this is what converts seconds to milliseconds, with 2.4% error)
+  TCCR1B |= (1 << CS12) | (1 << CS10);    // Set a prescaler of 1024 on Timer1 
   TIMSK1 |= (1 << OCIE1A);                // Enable Timer1 output compare match interrupt
   sei();                                  // Re-enable interrupts
 }
 
-ISR(TIMER1_COMPA_vect){ 
-/* Timer1 compare interrupt 
- * 
- */
-  //control();   
-}
-
-void parseData() {      // split the data into its parts
-
-    char * strtokIndx;                    // This is used by strtok() as an index
-
-    strtokIndx = strtok(temp_data,",");   // Get the first part - the string
-    strcpy(functionCall, strtokIndx);    // Copy it to function_call
-    
-  if(strcmp(functionCall,"set_parameters")==0){
-
+void set_parameters(){
     strtokIndx = strtok(NULL, ",");       // This continues where the previous call left off
-    Serial.println(atof(strtokIndx),4);     
+    band       = atof(strtokIndx);     
 
     strtokIndx = strtok(NULL, ",");
-    Serial.println(atof(strtokIndx),4);
+    t_i        = atof(strtokIndx);
     
     strtokIndx = strtok(NULL, ",");
-    Serial.println(atof(strtokIndx),4);
+    t_d        = atof(strtokIndx);
 
+    Serial.println(band,4);
+    Serial.println(t_i, 4);
+    Serial.println(t_d, 4);
   }
+
+void set_dac_voltage(){
+  strtokIndx = strtok(NULL, ",");
+  unsigned int voltage_12bit = atoi(strtokIndx);
+
+  if(mode == OPEN_LOOP){
+      //dac.setVoltage(voltage_12bit,false);
+      dac_output = voltage_12bit;
+      Serial.println(dac_output);    
+      return;
+    }
+    Serial.println("Arduino must be in OPEN_LOOP mode in order to directly manipulate the dac output.");
 }
+
+void set_mode(){
+
+    unsigned int _mode;
+    strtokIndx = strtok(NULL,",");
+    
+    if(strcmp(strtokIndx,"OPEN_LOOP") == 0){
+      mode = OPEN_LOOP  ;
+    }   
+    else if(strcmp(strtokIndx,"CLOSED_LOOP") == 0) {
+      mode = CLOSED_LOOP;
+    }
+    else{
+      Serial.println("Invaild Mode.");
+      return;
+    }
+    Serial.println(mode);
+  }
+/**
+void get_temperature(){
+   double thermistor_voltage = analogRead(THERMISTOR_PIN); 
+}
+
+void get_dac_voltage(){
+  return dac_voltage;
+}
+
+void get_mode(){
+  return mode;
+}
+*/
