edacc2efe3a22d47bb43e8e81a1357c676909b5c
Brandon Ruffolo <brandon.ruffolo@mcgill.ca>
2021-10-13 18:50:45 -0400

    Adding functionality

--- Temperature Controller/PID/PID.ino
+++ Temperature Controller/PID/PID.ino
@@ -1,8 +1,10 @@
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
+
 #define BAUD 9600
 #define THERMISTOR_PIN A2
- 
+
+/** Basic parameters **/ 
 double temperature; 
 double setpoint;
 double error;
@@ -20,13 +22,50 @@ unsigned int dac_output; // The MCP4725 is a 12-bit DAC, so this variable must b
 const byte data_size = 64;        // Size of the data buffer receiving from the serial line 
 char received_data[data_size];    // Array for storing received data
 char temp_data    [data_size];    // Temporary array for use when parsing
-char functionCall[20]  = {0};
-boolean newData = false;
+char functionCall[20]  = {0};     //
+boolean newData = false;          //
+char * strtok_index;              // Used by strtok() as an index
 
-
+/** Control Modes **/
 enum MODES{OPEN_LOOP,CLOSED_LOOP};
 enum MODES mode = OPEN_LOOP;
+const char *MODE_NAMES[] = {"OPEN_LOOP","CLOSED_LOOP"};
 
+/** Debugging **/
+bool _output = false;
+
+void control(){
+  /*
+   * 
+   * 
+   */
+  
+  long sum = 0, sumsq = 0;
+  int value;
+  const int N = 16;                 // Number of samples 
+  
+  for (int i = 0; i < N; i++) {     
+    value =  analogRead(THERMISTOR_PIN);    
+    sum += value;                           
+    sumsq += value*(long)value;             // Need to type cast to long because int is 16 bit 
+  }
+ 
+  double mu, sigma;
+  mu = sum / (double) N;            // Mean
+  sigma = sqrt((sumsq - sum*mu)/N); // Variance 
+
+  temperature = thermistor_temperature(mu);
+  error = (temperature - setpoint);
+
+  if (error > band/2) {
+    dac_output = 0;
+  } 
+  else if (error < -1*band/2) {
+    dac_output = 4095;
+  }
+  
+  if(_output){ dac.setVoltage(dac_output,false); }
+}
 
 void initialize(){
   setpoint = 25.0;
@@ -54,108 +93,27 @@ void loop() {
 }
 
 ISR(TIMER1_COMPA_vect){ 
-/* Timer1 compare interrupt 
- * 
+/* 
+ *  Timer1 compare interrupt 
  */
-  //control();   
+  control();   
 }
-
-
-char * strtokIndx;                    // This is used by strtok() as an index
 
 void parseData() {      // split the data into its parts
-    strtokIndx = strtok(temp_data,",");   // Get the first part - the string
-    strcpy(functionCall, strtokIndx);    // Copy it to function_call
+   strtok_index = strtok(temp_data,",");   // Get the first part - the string
+   strcpy(functionCall, strtok_index);     // Copy it to function_call
     
   if(strcmp(functionCall,"set_parameters")  == 0){ set_parameters();  }
-  if(strcmp(functionCall,"set_dac_voltage") == 0){ set_dac_voltage(); }
-  if(strcmp(functionCall,"set_mode")==0){ set_mode(); }
+  if(strcmp(functionCall,"set_dac")         == 0){ set_dac();         }
+  if(strcmp(functionCall,"set_mode")        == 0){ set_mode();        }
+  if(strcmp(functionCall,"set_period")      == 0){ set_period();      }
+  if(strcmp(functionCall,"get_dac")         == 0){ get_dac();         }
+  if(strcmp(functionCall,"get_mode")        == 0){ get_mode();        }
+  if(strcmp(functionCall,"get_temperature") == 0){ get_temperature(); }
+  if(strcmp(functionCall,"get_parameters")  == 0){ get_parameters();  }
 }
 
-
-
-void set_period(int period){     
-/*
- * Sets the time between calls to control().
- * 
- * period: period to be set in milliseconds.
- */    
-  unsigned int _number = floor(16e6*period/1024000) - 1; // Calculate the number of clock ticks in the specified period
-  
-  //char buf[256];
-  //sprintf(buf,"DEBUG\tsetPeriod(%d): number = %d, tick = %ld us, actual period = %ld us\n", period, number, (long)(1e3*tick), (long)(1e3*tick*(number+1)));
-  //Serial.print(buf);
-  
-  // Manipulating registers in the AVR chip (here the ATmega328 for the arduino uno), see the datasheet for details.
-  cli();                    // Disable interrupts
-
-  TCCR1A = 0;               // Blank out Timer control register A 
-  TCCR1B = 0;               // Blank out Timer control register B 
-  TCNT1  = 0;               // Initialize Timer1's counter value to be 0
-  OCR1A  = _number;         /* Set the output compare register 1 to the number of ticks found earlier */    
-                            /* Note that OCR1a is a 16-bit register, so _number <= 65,535             */ 
-
-  TCCR1B |= (1 << WGM12);                 // Enable clear timer on compare match (CTC) mode
-  TCCR1B |= (1 << CS12) | (1 << CS10);    // Set a prescaler of 1024 on Timer1 
-  TIMSK1 |= (1 << OCIE1A);                // Enable Timer1 output compare match interrupt
-  sei();                                  // Re-enable interrupts
+double thermistor_temperature(double thermistor_voltage){
+  // Write this !
+  return thermistor_voltage;
 }
-
-void set_parameters(){
-    strtokIndx = strtok(NULL, ",");       // This continues where the previous call left off
-    band       = atof(strtokIndx);     
-
-    strtokIndx = strtok(NULL, ",");
-    t_i        = atof(strtokIndx);
-    
-    strtokIndx = strtok(NULL, ",");
-    t_d        = atof(strtokIndx);
-
-    Serial.println(band,4);
-    Serial.println(t_i, 4);
-    Serial.println(t_d, 4);
-  }
-
-void set_dac_voltage(){
-  strtokIndx = strtok(NULL, ",");
-  unsigned int voltage_12bit = atoi(strtokIndx);
-
-  if(mode == OPEN_LOOP){
-      //dac.setVoltage(voltage_12bit,false);
-      dac_output = voltage_12bit;
-      Serial.println(dac_output);    
-      return;
-    }
-    Serial.println("Arduino must be in OPEN_LOOP mode in order to directly manipulate the dac output.");
-}
-
-void set_mode(){
-
-    unsigned int _mode;
-    strtokIndx = strtok(NULL,",");
-    
-    if(strcmp(strtokIndx,"OPEN_LOOP") == 0){
-      mode = OPEN_LOOP  ;
-    }   
-    else if(strcmp(strtokIndx,"CLOSED_LOOP") == 0) {
-      mode = CLOSED_LOOP;
-    }
-    else{
-      Serial.println("Invaild Mode.");
-      return;
-    }
-    Serial.println(mode);
-  }
-/**
-void get_temperature(){
-   double thermistor_voltage = analogRead(THERMISTOR_PIN); 
-}
-
-void get_dac_voltage(){
-  return dac_voltage;
-}
-
-void get_mode(){
-  return mode;
-}
-*/
---
+++ Temperature Controller/PID/get_and_set.ino
@@ -0,0 +1,88 @@
+void set_period(){     
+/*
+ * Sets the time between calls to control().
+ * 
+ * period: period to be set in milliseconds.
+ */    
+  strtok_index = strtok(NULL, ",");
+  unsigned int period = atoi(strtok_index);
+  unsigned int num_clk_ticks = floor(16e6*period/1024000) - 1; // Calculate the number of clock ticks in the specified period
+  
+  //char buf[256];
+  //sprintf(buf,"DEBUG\tsetPeriod(%d): number = %d, tick = %ld us, actual period = %ld us\n", period, number, (long)(1e3*tick), (long)(1e3*tick*(number+1)));
+  //Serial.print(buf);
+  
+  // Manipulating registers in the AVR chip (here the ATmega328 for the arduino uno), see the datasheet for details.
+  cli();                    // Disable interrupts
+
+  TCCR1A = 0;               // Blank out Timer control register A 
+  TCCR1B = 0;               // Blank out Timer control register B 
+  TCNT1  = 0;               // Initialize Timer1's counter value to be 0
+  OCR1A  = num_clk_ticks;   /* Set the output compare register 1 to the number of ticks found earlier */    
+                            /* Note that OCR1a is a 16-bit register, so _number <= 65,535             */ 
+
+  TCCR1B |= (1 << WGM12);                 // Enable clear timer on compare match (CTC) mode
+  TCCR1B |= (1 << CS12) | (1 << CS10);    // Set a prescaler of 1024 on Timer1 
+  TIMSK1 |= (1 << OCIE1A);                // Enable Timer1 output compare match interrupt
+  sei();                                  // Re-enable interrupts
+
+  Serial.println(period);
+}
+
+void set_parameters(){
+    strtok_index = strtok(NULL, ",");       // This continues where the previous call left off
+    band       = atof(strtok_index);     
+
+    strtok_index = strtok(NULL, ",");
+    t_i        = atof(strtok_index);
+    
+    strtok_index = strtok(NULL, ",");
+    t_d        = atof(strtok_index);
+  }
+
+void set_dac(){
+  strtok_index = strtok(NULL, ",");
+  unsigned int voltage_12bit = atoi(strtok_index);
+
+  if(mode == OPEN_LOOP){
+      //dac.setVoltage(voltage_12bit,false);
+      dac_output = voltage_12bit;
+      return;
+    }
+    Serial.println("Arduino must be in OPEN_LOOP mode in order to directly manipulate the dac output.");
+}
+
+void set_mode(){
+
+    unsigned int _mode;
+    strtok_index = strtok(NULL,",");
+    
+    if(strcmp(strtok_index,"OPEN_LOOP") == 0){
+      mode = OPEN_LOOP  ;
+    }   
+    else if(strcmp(strtok_index,"CLOSED_LOOP") == 0) {
+      mode = CLOSED_LOOP;
+    }
+    else{
+      Serial.println("Invaild Mode.");
+      return;
+    }
+  }
+
+void get_parameters(){
+  Serial.println(band,4);
+  Serial.println(t_i,4);
+  Serial.println(t_d,4);    
+}
+
+void get_dac(){
+  Serial.println(dac_output,4);
+}
+
+void get_mode(){
+  Serial.println(MODE_NAMES[mode]);
+}
+
+void get_temperature(){ 
+   Serial.println(temperature,4);
+}
