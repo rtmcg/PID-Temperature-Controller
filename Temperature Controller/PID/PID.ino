5bb1345f6facc215ac25f2d85f800561ca179a57
Brandon Ruffolo <brandon.ruffolo@mcgill.ca>
2021-12-07 12:28:10 -0500

    Added variables and math for calculating dt, the time differential needed for the control function.

--- Temperature Controller/PID/PID.ino
+++ Temperature Controller/PID/PID.ino
@@ -1,4 +1,3 @@
-
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
 #include <Adafruit_MAX31865.h>
@@ -12,16 +11,21 @@
 #define ENABLE_OUTPUT true 
 
 /** Basic parameters **/ 
-double temperature; 
-double setpoint;
-double error;
+double temperature; // Will hold the most currently recorded temperature from the RTD
+double setpoint;    // Temperature setpoint 
+double error;       // Temperature error relative to setpoint 
 
 /** PID control parameters **/
 double band        ;   // Proportional band
 double t_integral  ;   // Integral time
 double t_derivative;   // Derivative time
 
-unsigned int period;   // 1000 ms period 
+/** Timing parameters **/
+unsigned int period;   // Control period (in milliseconds)
+double dt;             // Time step between temperature measurements used in the control loop
+ 
+double time_control; // Time of the temperature measurement last used to update the control function   
+double time_recent ; // Time of the most recently taken temperature measurement
 
 /** Setup the external DAC **/
 Adafruit_MCP4725 dac;    // New DAC object
@@ -45,43 +49,47 @@ const char *MODE_NAMES[] = {"OPEN_LOOP","CLOSED_LOOP"};
 
 void control(){
   /*
-   * This is the control function used to change power 
-   * sent to the peltier based on the current setpoint and  
+   * This is the control function used to change the voltage 
+   * applied to the peltier based. 
+   * 
+   * Calculations of the control function are based on the current setpoint and  
    * most recently measured temperature.
-   */
-  /*
+   */ 
+  
   if (error >= band/2) {
     set_dac(4095);
   } 
   else if (error < -1*band/2) {
     set_dac(0);
-  }*/
-  double p = band*error + t_integral;
-
-  if(p>4095){p=4095;}
-
-  set_dac(p);
+  }
 }
 
 void initialize(){
   /*
-   * Initial control parameters 
+   * Initialalize control parameters 
    */
   set_setpoint(24.50);
-  set_parameters(1.0, 0, 0);
-  set_period(1000);
+  set_parameters(1.0, 1000.0, 1.0);
+  set_period(800);
   set_dac(0);
+
+  /* Doing a preliminary temperature reading */
+  temperature = rtd.temperature(RNOMINAL, RREF);                                        
+  time_control = millis(); 
+  time_recent  = millis();
 }
 
 void setup() {
   Serial.begin(BAUD);               
-  if(ENABLE_OUTPUT){
+  if(ENABLE_OUTPUT)
+  {
     pinMode(POLARITY_PIN, OUTPUT);    // Enable Polarity pin
     digitalWrite(POLARITY_PIN, LOW);  // Set Polarity pin LOW
     
     dac.begin(0x62);                  // Start communication with the external DAC
     dac.setVoltage(0, false);         // Set DAC output to ZERO
   }
+  
   rtd.begin(MAX31865_3WIRE);          // Begin SPI communcation with the MAX 31865 chip
   initialize();                       // Initialize relevant variables 
 }
@@ -97,22 +105,35 @@ void loop() {
       newData = false;                  // Reset newData flag
   }
   
-  read_temperature();
+  read_temperature();                  
 }
 
 void read_temperature(){
-  temperature = rtd.temperature(RNOMINAL, RREF); // One shot temperature measurement of the rtd 
-  error       = temperature - setpoint;          // 
+  /**
+   * Measure the RTD temperature, and update the temperature error and measurement time.
+   * 
+   * NOTE: This measurment takes in excess of 100-140 milliseconds to complete! 
+   * Take this into account if you wish to make your control period very small.
+   */
+  temperature             = rtd.temperature(RNOMINAL, RREF); // One shot temperature measurement of the rtd 
+  error                   = temperature - setpoint;          // Compute the temperature error.
+  time_recent = millis();                                    // Update the time that this measurement was taken
 }
 
 ISR(TIMER1_COMPA_vect){ 
 /* 
  *  Timer1 compare interrupt 
- *  This interrupt is called at regular intervals. This interval can be set with the set_period() function.  
+ *  
+ *  This interrupt is called at a regular intervals, that can be set with the set_period() function.  
  *  The main function of this interrupt is to call the control() function at fixed time intervals.
+ *  
+ *  NOTE: This interupt will not be active until set_period() is called!
  */
- 
+  
   if(mode == CLOSED_LOOP){
+    dt = time_recent - time_control; // Update the time differential
+    time_control = time_recent;      // Update the time of the temperature measurement used in the control loop
+  
     interrupts(); /* Re-enable interrupts (allowing nested interrupts) */
                   /* Interrupts are needed to communicate with the DAC (MCP4725) via I2C */
                   
