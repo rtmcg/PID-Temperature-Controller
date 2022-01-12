7ed05a67b021b6455b00cc90a4652c03d88736df
Brandon Ruffolo <brandon.ruffolo@mcgill.ca>
2022-01-12 15:09:37 -0500

    File reorg

---
+++ Temperature Controller/Arduino/get_and_set.ino
@@ -0,0 +1,99 @@
+#define sgn(x) ((x) < 0 ? -1 : ((x) > 0 ? 1 : 0)) // Gets the sign (positive or negative) of the argument
+
+void set_period(unsigned int _period){     
+/*
+ * Sets the time between calls to control().
+ * 
+ * period: period to be set in milliseconds.
+ */    
+  unsigned int num_clk_ticks = floor(16e6*_period/1024000) - 1; // Calculate the number of clock ticks in the specified period
+  
+  // Manipulating registers in the AVR chip (here the ATmega328 for the arduino uno), see the datasheet for details.
+  cli();                    // Disable interrupts
+
+  TCCR1A = 0;               // Blank out Timer control register A 
+  TCCR1B = 0;               // Blank out Timer control register B 
+  TCNT1  = 0;               // Initialize Timer1's counter value to be 0
+  OCR1A  = num_clk_ticks;   /* Set the output compare register 1 to the number of ticks calculated earlier */    
+                            /* Note that OCR1a is a 16-bit register, so _number <= 65,535             */ 
+
+  TCCR1B |= (1 << WGM12);                 // Enable clear timer on compare match (CTC) mode
+  TCCR1B |= (1 << CS12) | (1 << CS10);    // Set a prescaler of 1024 on Timer1 
+  TIMSK1 |= (1 << OCIE1A);                // Enable Timer1 output compare match interrupt
+  sei();                                  // Re-enable interrupts
+
+  period = _period;
+}
+
+void set_parameters(float _band, float _t_integral, float _t_derivative){
+    
+    band         = _band;
+    t_integral   = _t_integral;
+    t_derivative = _t_derivative;   
+  }
+
+void set_dac(int voltage_12bit){
+  
+  if(ENABLE_OUTPUT){
+    
+    // Check if a polarity change is needed
+    if (sgn(dac_output) != sgn(voltage_12bit)){
+
+      // Bring the dac output to zero temporarily for safe polarity change
+      dac.setVoltage(0,false);
+
+      // No need to change sign if we are turning the output off
+      if(voltage_12bit == 0){ 
+         dac_output = voltage_12bit;
+         return;
+      }
+
+      // Flip the polarity pin accordingly
+      if(voltage_12bit < 0) digitalWrite(POLARITY_PIN,HIGH);
+      else                  digitalWrite(POLARITY_PIN,LOW);
+    }
+
+    // Set the dac to desired output
+    dac.setVoltage(abs(voltage_12bit),false);
+  }
+  
+  // Save the full signed output 
+  dac_output = voltage_12bit;
+}
+
+void set_mode(MODES _mode){
+    mode = _mode;
+}
+
+void set_setpoint(float _setpoint){
+  /*
+   * Set Temperature setpoint.
+   */
+  setpoint = _setpoint;
+}
+
+int get_dac(){
+  return dac_output;
+}
+
+MODES get_mode(){
+  return mode;
+}
+
+float get_temperature(){
+  /* 
+   *  Get the most recently measured temperature.
+   */
+  return temperature;
+}
+
+float get_setpoint(){
+  /*
+   * Get the current temperature setpoint.
+   */
+  return setpoint;
+}
+
+int get_period(){
+  return period;
+}
---
+++ Temperature Controller/Arduino/serial_data.ino
@@ -0,0 +1,142 @@
+/**
+ * Based on "Serial Input Basics" by user Robin2 on Arduino forums.
+ * See https://forum.arduino.cc/t/serial-input-basics-updated/382007
+ */
+
+const char startMarker = '<';
+const char endMarker   = '>'; 
+
+void receive_data() {
+  static boolean recv_in_progress = false;
+  static byte index = 0;
+  char rc;
+  
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+   
+    if (recv_in_progress == true) {
+      
+      if (rc != endMarker) {
+        received_data[index] = rc;
+        index++;
+        
+        if (index >= data_size) {
+          index = data_size - 1;
+          // Send warning to user that data buffer is full.
+        }
+      }
+      else {
+        received_data[index] = '\0'; // terminate the string
+        recv_in_progress = false;
+        index = 0;
+        newData = true;
+        //Serial.println(received_data);
+      }
+    }
+
+    else if (rc == startMarker) {
+        recv_in_progress = true;
+    }
+  }
+}
+
+void parseData() {      
+   strtok_index = strtok(temp_data,",");   // Get the first part - the string
+   strcpy(functionCall, strtok_index);     // Copy it to function_call
+   strtok_index = strtok(NULL, ",");
+    
+  if(strcmp(functionCall,"set_parameters") == 0){
+    float _band       = atof(strtok_index);     
+
+    strtok_index      = strtok(NULL, ",");
+    float _t_i        = atof(strtok_index);
+    
+    strtok_index      = strtok(NULL, ",");
+    float _t_d        = atof(strtok_index);
+
+    set_parameters(_band, _t_i, _t_d);
+  }
+  
+  if(strcmp(functionCall,"set_dac") == 0){ 
+    int voltage_12bit = atoi(strtok_index);
+
+    if(mode == OPEN_LOOP){
+      set_dac(voltage_12bit);
+      return;
+    }
+    Serial.println("Arduino must be in OPEN_LOOP mode in order to directly manipulate the dac output.");
+  }
+  
+  if(strcmp(functionCall,"set_mode")        == 0){
+
+    if(strcmp(strtok_index,"OPEN_LOOP") == 0){
+      set_mode(OPEN_LOOP);
+    }   
+    else if(strcmp(strtok_index,"CLOSED_LOOP") == 0) {
+      set_mode(CLOSED_LOOP);
+    }
+    else{
+      Serial.println("Invaild Mode.");
+      return;
+    }
+  }
+  
+  if(strcmp(functionCall,"set_period")      == 0){
+    set_period(atoi(strtok_index));
+  }
+  
+  if(strcmp(functionCall,"set_setpoint")    == 0){      
+    set_setpoint(atof(strtok_index));    
+  }
+  
+  if(strcmp(functionCall,"get_dac")         == 0){ 
+      Serial.println(get_dac());
+  }
+  
+  if(strcmp(functionCall,"get_mode")        == 0){ 
+    MODES _mode = get_mode();
+    if(_mode == CLOSED_LOOP){
+      Serial.println("CLOSED_LOOP");        
+    }
+    else{
+      Serial.println("OPEN_LOOP");
+    }
+  }
+  
+  if(strcmp(functionCall,"get_temperature") == 0){
+    float _temperature = get_temperature();
+    Serial.println(_temperature,2);
+  }
+  
+  if(strcmp(functionCall,"get_parameters")  == 0){
+    Serial.print(band,4);
+    Serial.print(',');
+    Serial.print(t_integral,4);
+    Serial.print(',');
+    Serial.println(t_derivative,4);   
+  }
+  
+  if(strcmp(functionCall,"get_setpoint")    == 0){ 
+    Serial.println(get_setpoint(),4);    
+  }
+  
+  if(strcmp(functionCall,"get_period")    == 0){ 
+    Serial.println(get_period());    
+  }
+
+  if(strcmp(functionCall,"get_all_variables") == 0){
+    Serial.print(get_temperature(),2);
+    Serial.print(',');
+    Serial.print(get_setpoint(),4);
+    Serial.print(',');
+    Serial.print(get_dac());
+    Serial.print(',');
+    Serial.print(band,4);
+    Serial.print(',');
+    Serial.print(t_integral,4);
+    Serial.print(',');
+    Serial.print(t_derivative,4); 
+    Serial.print(',');
+    Serial.println(get_period()); 
+  }
+}
